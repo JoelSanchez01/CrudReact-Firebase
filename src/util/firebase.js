1086258454b971dbbf0a69973e1ee8761fbe879b
JoelSanchez01 <joeleduardosanchezherrera@gmail.com>
2021-07-27 00:32:00 -0500

    Se borraron los datos privados de firebase

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,12 +1,6 @@
 import firebase from 'firebase';
 const firebaseConfig = {
-    apiKey: "AIzaSyAcG8SJ67gqWbXDWN7oAhR3Ax4-NNaHJBM",
-    authDomain: "react-demo-ace53.firebaseapp.com",
-    databaseURL: "https://react-demo-ace53-default-rtdb.firebaseio.com",
-    projectId: "react-demo-ace53",
-    storageBucket: "react-demo-ace53.appspot.com",
-    messagingSenderId: "968561313279",
-    appId: "1:968561313279:web:190531b64f25c4044593ca"
+
   };
   
 firebase.initializeApp(firebaseConfig);
